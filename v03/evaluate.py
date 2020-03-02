635ec9ec993ebc47d311a07f69286fd869b2a1d7
tt <traitran44@gmail.com>
2020-03-01 16:21:02 -0800

    Added build graph for tf and copy/paste fill_genome() into build_graph()

--- v03/evaluate.py
+++ v03/evaluate.py
@@ -107,100 +107,113 @@ class TfGraphEvaluateDefinition(EvaluateDefinition):
         self.operators, self.operator_weights = operators, operator_weights
         self.operator_dict, self.genome_main_count, self.genome_output_dtypes = operator_dict, genome_main_count, genome_output_dtypes
 
-    def random_ftn(self, req_dtype=None, exclude=[], return_all=False):
-        '''
-        words
-        '''
-        choices = np.array(self.operators)
-        weights = np.array(self.operator_weights)
-
-        for val in exclude:
-            # note: have to delete from weights first because we use choices to get right index
-            weights = np.delete(weights, np.where(choices == val))
-            choices = np.delete(choices, np.where(choices == val))
-
-        # now check the output dtypes match
-        if req_dtype is not None:
-            delete = []
-            for ith_choice, choice in enumerate(choices):
-                if self.operator_dict[choice]["output"] != req_dtype:
-                    delete.append(ith_choice)
-            weights = np.delete(weights, delete)
-            choices = np.delete(choices, delete)
-
-        if weights.sum() < 1 - 1e-3:
-            # we must have removed some values...normalize
-            weights *= 1 / weights.sum()
-
-        if return_all:
-            return rnd.choice(choices, size=len(choices), replace=False, p=weights)
-        else:
-            return rnd.choice(choices, p=weights)
-
-    def get_node_dtype(self, block, node_index: int, key: str):
-        '''
-        key returns that key-value from the respective node_dictionary
-         * "inputs"
-         * "args"
-         * "output"
-        '''
+    def get_node_type(self, node_index, arg_dtype=False, input_dtype=False, output_dtype=False):
         if node_index < 0:
-            # input_node
-            return self.input_dtypes[-1 * node_index - 1]
-        elif node_index >= self.main_count:
-            # output_node
-            return self.output_dtypes[node_index - self.main_count]
+            # then it's a Block Input Node
+            return self.genome_input_dtypes[-1*node_index-1] # -1-->0, -2-->1, -3-->2
+        elif node_index >= self.genome_main_count:
+            # then it's a Block Output Node
+            return self.genome_output_dtypes[node_index-self.genome_main_count]
         else:
-            # main_node
-            node_ftn = block[node_index]["ftn"]
-            oper_dict_value = self.operator_dict[node_ftn]
-            return oper_dict_value[key]
+            # then it's a Block Main Node
+            pass
+        try:
+            ftn = self[node_index]["ftn"]
+        except TypeError:
+            print('Input/output dtype is incompatible with operator functions')
+            quit()
+        ftn_dict = self.operator_dict[ftn]
+        if input_dtype:
+            # get the required input data types for this function
+            return ftn_dict["inputs"] # will return a list
+        elif output_dtype:
+            # get the output data types for this function
+            return ftn_dict["outputs"] # returns a single value, not a list...arity is always 1 for outputs
+        elif arg_dtype:
+            return ftn_dict["args"]
+        else:
+            print("ProgrammerError: script writer didn't assign input/output dtype for getNodeType method")
+            exit()
 
-    def random_input(self, block, req_dtype, min_=None, max_=None, exclude=[]):
-        '''
-        note max_ is exclusive so [min_,max_)
 
-        return None if we failed to find good input
-        '''
-        if min_ is None:
-            min_ = -1 * self.input_count
-        if max_ is None:
-            max_ = self.main_count
+    def random_ftn(self, only_one=False, exclude=None, output_dtype=None):
+        choices = self.ftn_methods
+        weights = self.ftn_weights
+        if exclude is not None:
+            delete = []
+            for val in exclude:
+                # value == list doesn't work when the value is a function
+                for c, choice in enumerate(choices):
+                    if val==choice:
+                        delete.append(c)
 
+            if len(choices) != 1:
+                #print("Should not call mutate since only one function available. Returning original function")
+
+                choices = np.delete(choices, delete)
+                weights = np.delete(weights, delete)
+                weights /= weights.sum() # normalize weights
+        else:
+            pass
+        if output_dtype is not None:
+            # force the selected function to have the output_dtype
+            delete = []
+            for c, choice in enumerate(choices):
+                if self.operator_dict[choice]["outputs"]!=output_dtype:
+                    delete.append(c)
+            choices = np.delete(choices, delete)
+            weights = np.delete(weights, delete)
+            weights /= weights.sum() # normalize weights
+        if only_one:
+            return np.random.choice(a=choices, size=1, p=weights)[0]
+        else:
+            return np.random.choice(a=choices, size=len(self.ftn_methods), replace=False, p=weights)
+
+
+    def random_input(self, dtype, min_='default', max_='default', exclude=None):
+        # max_ is one above the largest integer to be drawn
+        # so if we are randomly finding nodes prior to a given node index, set max_ to that index
+        if min_=='default':
+            min_=-1*self.genome_input_count
+        if max_=='default':
+            max_=self.genome_main_count
         choices = np.arange(min_, max_)
-        for val in exclude:
-            choices = np.delete(choices, np.where(choices == val))
-
-        if len(choices) == 0:
-            # nothing left to choose from
-            return None
+        if exclude is not None:
+            for val in exclude:
+                choices = np.delete(choices, np.where(choices==val))
         else:
-            '''
-            exhuastively try each choice to see if we can get datatypes to match
-            '''
-            poss_inputs = np.random.choice(a=choices, size=len(choices), replace=False)
-            for input_index in poss_inputs:
-                input_dtype = self.get_node_dtype(block, input_index, "output")
-                if req_dtype == input_dtype:
-                    return input_index
-                else:
-                    pass
-            # none of the poss_inputs worked, failed to find matching input
+            pass
+        if len(choices) == 0:
+            #nothing to choices from...very rare but very possible
             return None
+        possible_nodes = np.random.choice(a=choices, size=len(choices), replace=False)
+        # iterate through each input until we find a datatype that matches dtype
+        for poss_node in possible_nodes:
+            poss_node_outputdtype = self.get_node_type(node_index=poss_node, output_dtype=True)
+            # note, we currently only allow for arity of 1 for our ftns...so getNodeType will return a value not a list if output_dtype is True
+            if dtype == poss_node_outputdtype:
+                return poss_node
+            else:
+                pass
+        # if we got this far then we didn't find a match
+        return None
 
-    def random_arg(self, req_dtype, exclude=[]):
-        '''
-        words
-        '''
+
+    def random_arg(self, dtype, exclude=None):
+        # have to assume here that each arg_dtype will have at least two in each self.args...or else when we mutate() we would get same arg
         choices = []
-        for arg_index, arg_type in enumerate(self.arg_types):
-            if (arg_type == req_dtype) and (arg_index not in exclude):
+        for arg_index in range(self.args_count):
+            arg_dtype = type(self.args[arg_index]).__name__
+            if arg_dtype == dtype:
+                if (exclude is not None) and (arg_index in exclude):
+                    continue
                 choices.append(arg_index)
-
         if len(choices) == 0:
-            return None
+            print("UserInputError: A ftn was provided without having its required (or invalid) data type in the arguments")
+            exit()
         else:
-            return rnd.choice(choices)
+            return np.random.choice(a=choices, size=1)[0]
+
 
     def build_graph(self):
         genome = []
@@ -214,7 +227,7 @@ class TfGraphEvaluateDefinition(EvaluateDefinition):
                 input_dtypes = self.operator_dict[ftn]["inputs"]
                 input_nodes = []  # [None]*len(input_dtypes)
                 for input_dtype in input_dtypes:
-                    input_nodes.append(self.random_input(max_=node_index, req_dtype=input_dtype))
+                    input_nodes.append(self.random_input(max_=node_index, dtype=input_dtype))
                 if None in input_nodes:
                     # couldn't find a matching input + output value. try the next ftn
                     break
@@ -225,7 +238,7 @@ class TfGraphEvaluateDefinition(EvaluateDefinition):
                 arg_dtypes = self.operator_dict[ftn]["args"]
                 arg_nodes = []
                 for arg_dtype in arg_dtypes:
-                    arg_nodes.append(self.random_arg(req_dtype=arg_dtype))
+                    arg_nodes.append(self.random_arg(dtype=arg_dtype))
                 # assign all values to the genome
                 genome[node_index] = {"ftn": ftn,
                                       "inputs": input_nodes,
