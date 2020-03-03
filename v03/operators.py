a502b68520ff4e41845d7adb070d6a35b7d07c11
michael <michaeljurado42@gmail.com>
2020-03-02 20:41:50 -0500

    reverted utils.py operators and simple_numpy.py in order to make branch compatible with Rodd's change

--- v03/operators.py
+++ v03/operators.py
@@ -1,10 +1,5 @@
 '''
-OperatorDefinition:
-
-Definition for the Operators interface, which collects primitives from various user-defined modules and creates operator-weight pairs that will be used to initialize genome.
-
-Specific problems (e.g. symbolic regression) are implemented as an example on how to initiate an Operators set definition. 
-
+words
 '''
 
 # packages
@@ -17,63 +12,54 @@ from utils import build_weights
 import simple_numpy
 
 
+
 class OperatorDefinition():
     '''
-    An interface that outlines the data structure that stores operators for a given machine learning task (e.g. primitive neural network layers, functions)
+    words
     '''
     def __init__(self,
+                operators: List,
+                weights: List,
                 modules: List):
-        '''
-        :param modules: the filenames from modules
-        '''
         self.build_operDict(modules)
-        self.build_weights()
+        self.operators = operators
+        self.weights = weights
+
 
     def build_operDict(self, modules: List):
         '''
-        Import the operator dict from every module in the list and return
-
-        Creates a single operator dictionary from all of the individual modules
+        import the operator dict from every module in the list and return
         '''
         self.operator_dict = {}
-        for file_name in modules:
-            module = __import__(file_name)
-            self.operator_dict.update(moduel.operDict)
-            del module 
+        for oper_py in modules:
+            _ = __import__(oper_py)
+            self.operator_dict.update(_.operDict)
+            del _
 
-    def build_weights(self):
-        '''
-        Converts weights into normalized relative weights and reassigns them to self.operator_dict
-        '''
-        weights = []
-        for operator in self.operator_dict:
-            weights.append(operator['weight'])
-
-        # cast to numpy to normalize
-        weights = np.array(weights) 
-
-        # normalize the weights
-        weights /= np.sum(weights)
-
-        # reassign the weights to operators dictionary
-        for operator, weight in self.operator_dict, weights:
-           operator['weight'] = weight 
 
 class SymbRegressionNoArgs(OperatorDefinition):
     '''
-    Operator definition for the Symbolic Regression problem
+    words
     '''
     def __init__(self):
-        # example module user defines for symbolic regression operators
-        modules = ['symbolic_regression_operators'] 
+        modules = ['simple_numpy']
+        weight_dict = {simple_numpy.add_ff2f: 1,
+                    simple_numpy.add_fa2a: 1,
+                    simple_numpy.add_aa2a: 1,
+                    simple_numpy.sub_ff2f: 1,
+                    simple_numpy.sub_fa2a: 1,
+                    simple_numpy.sub_aa2a: 1,
+                    simple_numpy.mul_ff2f: 1,
+                    simple_numpy.mul_fa2a: 1,
+                    simple_numpy.mul_aa2a: 1}
+        operators, weights = build_weights(weight_dict)
         OperatorDefinition.__init__(self,
+                                    operators,
+                                    weights,
                                     modules)
-
 '''
 class SymbRegressionWithArgs(OperatorDefinition):
-
     #words
-
     def __init__(self):
         modules = ['simple_numpy']
         weight_dict = {simple_numpy.add_aa2a: 1,
--- v03/simple_numpy.py
+++ v03/simple_numpy.py
@@ -2,39 +2,32 @@
 import numpy as np
 operDict = {}
 
-'''
-Each of these functions are operators for the basic "symbolic_regression_operators" module used in Symbolic Regression.
-
-The operDict that defines the metadata for each of these primitives are outlined as follows:
-
-    inputs: type of the arguments
-    output: type of the output
-    weight: likelihood operator is chosen when initializing genome
-'''
 
 def add_ff2f(a,b):
     return np.add(a,b)
-
 operDict[add_ff2f] = {"inputs": [np.float64, np.float64],
                       "output": np.float64,
-                      "args": [],
-                      "weight": 1
+                      "args": []
                     }
 
 def add_fa2a(a,b):
     return np.add(a,b)
 operDict[add_fa2a] = {"inputs": [np.ndarray, np.float64],
                       "output": np.ndarray,
-                      "args": [],
-                      "weight": 1
+                      "args": []
                       }
+"""
+operDict[add_fa2a] = {"inputs": [np.ndarray],
+                      "output": np.ndarray,
+                      "args": [FloatSmall],
+                      "num_args": 3}
+"""
 
 def add_aa2a(a,b):
     return np.add(a,b)
 operDict[add_aa2a] = {"inputs": [np.ndarray, np.ndarray],
                       "output": np.ndarray,
-                      "args": [],
-                      "weight": 1
+                      "args": []
                       }
 
 
@@ -42,47 +35,56 @@ def sub_ff2f(a,b):
     return np.subtract(a,b)
 operDict[sub_ff2f] = {"inputs": [np.float64, np.float64],
                       "output": np.float64,
-                      "args": [],
-                      "weight": 1
+                      "args": []
                       }
 
 def sub_fa2a(a,b):
     return np.subtract(a,b)
 operDict[sub_fa2a] = {"inputs": [np.float64, np.ndarray],
                       "output": np.ndarray,
-                      "args": [],
-                      "weight": 1
+                      "args": []
                       }
+#operDict[sub_fa2a] = {"inputs": [np.ndarray],
+#                     "output": np.ndarray,
+#                     "args": [FloatSmall],
+#                     "num_args": 3}
 
 def sub_aa2a(a,b):
     return np.subtract(a,b)
 operDict[sub_aa2a] = {"inputs": [np.ndarray, np.ndarray],
                       "output": np.ndarray,
-                      "args": [],
-                      "weight": 1
+                      "args": []
                       }
+#operDict[sub_aa2a] = {"inputs": [np.ndarray, np.ndarray],
+#                     "output": np.ndarray,
+#                     "args": [],
+#                     "num_args": 3}
 
 def mul_ff2f(a,b):
     return np.multiply(a,b)
 operDict[mul_ff2f] = {"inputs": [np.float64, np.float64],
                       "output": np.float64,
-                      "args": [],
-                      "weight": 1
+                      "args": []
                       }
 
 def mul_fa2a(a,b):
     return np.multiply(a,b)
 operDict[mul_fa2a] = {"inputs": [np.float64, np.ndarray],
                       "output": np.ndarray,
-                      "args": [],
-                      "weight": 1
+                      "args": []
                       }
+#operDict[mul_fa2a] = {"inputs": [np.ndarray],
+#                     "output": np.ndarray,
+#                     "args": [FloatSmall],
+#                     "num_args": 3}
 
 def mul_aa2a(a,b):
     return np.multiply(a,b)
 operDict[mul_aa2a] = {"inputs": [np.ndarray, np.ndarray],
                       "output": np.ndarray,
-                      "args": [],
-                      "weight": 1
+                      "args": []
                       }
-
+#operDict[mul_aa2a] = {"inputs": [np.ndarray, np.ndarray],
+#                     "output": np.ndarray,
+#                     "args": [],
+#                     "num_args": 3}
